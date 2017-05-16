331241f0c21cc1e47bcea7cc3b290cc4e32c5c6b
mwweinberg <mwweinberg@users.noreply.github.com>
2017-05-15 20:51:21 -0400

    added process print statements
    Now when it runs it prints out what it is doing.

--- sxswshuffle.py
+++ sxswshuffle.py
@@ -36,11 +36,15 @@ jessica_input_cleaned = ([s.strip('jessica_input/') for s in jessica_input])
 ####Sort file names into lists
 ###########################
 
+print(" ")
+print("Creating the list from Michael music.")
+print(" ")
 #analyize input1
 #for the first one you need to add files to the shared folder too
 for root, dirs, files in os.walk("michael_input", topdown=False):
     #for each file in input1
     for name in files:
+        print(f"Listing {name}")
         #see if it is in input2_cleaned
         if name in jessica_input_cleaned:
             #if it is addit to shared
@@ -49,11 +53,16 @@ for root, dirs, files in os.walk("michael_input", topdown=False):
             #if it isn't add it to input1_only
             michael_input_only.append(name)
 
+
+print(" ")
+print("Creating the list from Jessica music.")
+print(" ")
 #analyize input2
 #since shared is already built, only need to copy input2_only
 for root, dirs, files in os.walk("jessica_input", topdown=False):
     #for each file in input2
     for name in files:
+        print(f"Listing {name}")
         #see if it is in input1_cleaned
         if name not in michael_input_cleaned:
             jessica_input_only.append(name)
@@ -62,11 +71,14 @@ for root, dirs, files in os.walk("jessica_input", topdown=False):
 #############################
 #####Sort files
 ##############################
-
+print(" ")
+print("Sorting Michael music.")
+print(" ")
 #compares files against the lists and copies accordingly
 #for files in input 1
 for root, dirs, files in os.walk("michael_input", topdown=False):
     for filename in files:
+        print(f"Sorting {filename}")
         #if the file shows up in the input1_only list
         if filename in michael_input_only:
             #this creates the output filename (includes director)
@@ -81,8 +93,13 @@ for root, dirs, files in os.walk("michael_input", topdown=False):
         else:
             print(f"There was an error with {filename}")
 
+print(" ")
+print("Sorting Jessica music.")
+print(" ")
+
 for root, dirs, files in os.walk("jessica_input", topdown=False):
     for filename in files:
+        print(f"Sorting {filename}")
         if filename in jessica_input_only:
             jessica_out = "jessica_out/" + filename
             shutil.copy(os.path.join(root, filename), jessica_out)
@@ -101,10 +118,14 @@ for root, dirs, files in os.walk("jessica_input", topdown=False):
 
 #loads the cover art
 imagedata = open("sxsw2016.jpg", "rb").read()
+print(" ")
+print("Fixing the album for Jessica only.")
+print(" ")
 
 #rename the album for Jessica only files
 for root, dirs, files in os.walk("jessica_out/", topdown=False):
     for filename in files:
+        print(f"Fixing {filename}")
         #create the name of the file that includes the dir and the filename
         alt_name = "jessica_out/" + filename
         #create teh eyed3 object based on that file name
@@ -117,19 +138,32 @@ for root, dirs, files in os.walk("jessica_out/", topdown=False):
         #save the change
         new_audio.tag.save()
 
+print(" ")
+print("Fixing the album for Michael only.")
+print(" ")
+
 #rename the album for Michael only files
 for root, dirs, files in os.walk("michael_out/", topdown=False):
     for filename in files:
+        print(f"Fixing {filename}")
         alt_name = "michael_out/" + filename
         new_audio = eyed3.load(alt_name)
         new_audio.tag.album = u"SXSW 2016 Showcasing Artist (Michael)"
         new_audio.tag.images.set(3,imagedata,"image/jpeg")
         new_audio.tag.save()
 
+print(" ")
+print("Fixing the album for shared music.")
+print(" ")
 #this just adds the album art because the name is right already
 for root, dirs, files in os.walk("shared_out/", topdown=False):
     for filename in files:
+        print(f"Fixing {filename}")
         alt_name = "shared_out/" + filename
         new_audio = eyed3.load(alt_name)
         new_audio.tag.images.set(3,imagedata,"image/jpeg")
         new_audio.tag.save()
+
+print(" ")
+print("Done!")
+print(" ")
